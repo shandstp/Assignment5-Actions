26d7f69d6e00be195b4273e033c0721656e13ba6
Travis Shands <fromthecrossroad@gmail.com>
2020-02-29 16:58:02 -0800

    radius test

--- task.py
+++ task.py
@@ -1,10 +1,6 @@
 import math
 
 
-def firstrun():
-    return "success"
-
-
 def areaFromRadius(radius):
     area = 3.14 * math.pow(radius, 2)
     return area
--- tests.py
+++ tests.py
@@ -3,14 +3,6 @@ import task
 
 
 class TestCase(unittest.TestCase):
-    def test1(self):
-        expected = "success"
-        self.assertEqual(expected, task.firstrun())
-
-    def test2(self):
-        expected = "failure"
-        self.assertNotEqual(expected, task.firstrun())
-
     def test_areaFromRadius(self):
         self.assertEqual(78.5, task.areaFromRadius(5))
 
